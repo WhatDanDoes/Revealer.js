d35c7e060ec98a68d6326a2a4fc8422ecc69bf61
Daniel Bidulock <daniel@capitolhill.ca>
2019-02-27 15:39:22 -0700

    Make authentication configurable through .env

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    ioServer = require('socket.io'),
-    app = express(),
-    masterUser = 'username',
-    masterPass = 'password',
-    port = process.env.PORT || 3001;
+require('dotenv').config()
+const express = require('express'),
+      http = require('http'),
+      path = require('path'),
+      ioServer = require('socket.io'),
+      app = express(),
+      masterUser = 'username',
+      masterPass = 'password',
+      port = process.env.PORT || 3001;
 
-
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Authentication
+const users = {};
+users[process.env.USERNAME] = process.env.PASSWORD;
 const auth = require('express-basic-auth')({
-  users: { 'admin': 'supersecret' },
+  users: users,
   challenge: true
 });
- 
+
 app.get('/', auth, function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+  res.render('index');
 });
 
 app.get('/client', function (req, res) {
-    res.sendFile(__dirname + '/views/client.html');
+  res.render('client');
 });
 
-var server = http.createServer(app).listen(port, function () {
-    console.log("Express server listening on port "+port);
+const server = http.createServer(app).listen(port, function () {
+  console.log("Express server listening on port " + port);
 });
 
-var io = ioServer.listen(server);
+const io = ioServer.listen(server);
 io.sockets.on('connection', function (socket) {
-    socket.emit("message", "Welcome to Revealer");
-    socket.on("slidechanged", function (data) {
-        socket.broadcast.emit("slidechanged", data);
-    });
+  socket.emit("message", "Welcome to Revealer");
+  socket.on("slidechanged", function (data) {
+    socket.broadcast.emit("slidechanged", data);
+  });
 });
